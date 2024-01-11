1b7180cc451cde62018e35ddf4c8aecbdbc78949
Jakub Kopański <jakub@famisoft.pl>
2024-01-11 14:08:48 +0100

    Parse all of the syntax

--- lib/parser/mod.rs
+++ lib/parser/mod.rs
@@ -1,43 +1,54 @@
-use std::{ops::Range, todo, println};
-use miette::{
-    Diagnostic,
-    SourceOffset,
-    SourceSpan,
+use crate::syntax;
+use miette::{Diagnostic, SourceOffset, SourceSpan};
+use std::{
+    boxed::Box,
+    collections::HashMap,
+    io::{Cursor, Read, Seek, SeekFrom},
+    num::TryFromIntError,
+    ops::Range,
 };
 use thiserror::Error;
 use tree_sitter::Node;
 use tree_sitter_pie as pie;
-use crate::syntax as syntax;
 
 type Result<T> = std::result::Result<T, ParseError>;
-type Parser<T> = fn(&Node) -> Result<T>;
+
+// TODO: make proper type for parsers
+#[allow(type_alias_bounds)]
+type Parser<T, S: Read + Seek> = fn(&Node, &mut S) -> Result<T>;
 
 #[derive(Debug, Diagnostic, Error)]
 #[error("Unexpected token")]
 #[diagnostic(
-    help("encountered `{}' when `{}' was expected", self.actual, self.expected)
+    help("encountered `{}' when {} was expected", self.actual, self.expected)
 )]
 pub struct Mismatch {
     #[label("here")]
     pub loc: SourceSpan,
     pub actual: String,
-    pub expected: &'static str,
+    pub expected: String,
 }
 
 #[derive(Debug, Diagnostic, Error)]
-#[error("Unexpected token")]
+#[error("Missing data")]
 #[diagnostic(
-    help(
-	"encountered `{}' but I was expecting one of: {}",
-	self.actual,
-	self.expected.iter().fold(String::new(), |acc, msg| { format!("`{msg}', {acc}") })
-    )
+    help("Couldn't extract {} for {}", self.what, self.token)
 )]
-pub struct Choice {
-    #[label("here")]
+pub struct Missing {
+    #[label("this")]
     pub loc: SourceSpan,
-    pub actual: String,
-    pub expected: Vec<&'static str>,
+    pub token: String,
+    pub what: String,
+}
+
+#[derive(Debug, Diagnostic, Error)]
+#[error("Couldn't read source")]
+#[diagnostic()]
+pub struct Reading {
+    #[label("this thing right here")]
+    pub loc: SourceSpan,
+    #[help]
+    pub help: String,
 }
 
 #[derive(Debug, Diagnostic, Error)]
@@ -48,31 +59,41 @@ pub enum ParseError {
     TreeSitter(#[from] tree_sitter::LanguageError),
     #[error(transparent)]
     #[diagnostic(transparent)]
+    Reading(#[from] Reading),
+    #[error(transparent)]
+    #[diagnostic(transparent)]
     Mismatch(#[from] Mismatch),
     #[error(transparent)]
     #[diagnostic(transparent)]
-    Choice(#[from] Choice),
+    Missing(#[from] Missing),
 }
 
-fn choice(
-    loc: SourceSpan,
-    actual: String,
-    expected: Vec<&'static str>
-) -> ParseError {
-    ParseError::Choice(Choice { loc, actual, expected })
+fn mismatch(loc: SourceSpan, actual: String, expected: String) -> ParseError {
+    ParseError::Mismatch(Mismatch {
+        loc,
+        actual,
+        expected,
+    })
 }
 
-fn mismatch(loc: SourceSpan, actual: String, expected: &'static str) -> ParseError {
-    ParseError::Mismatch(Mismatch { loc, actual, expected })
+fn missing(loc: SourceSpan, token: String, what: String) -> ParseError {
+    ParseError::Missing(Missing { loc, token, what })
+}
+
+fn one_of_msg<S: std::fmt::Display>(variants: impl ExactSizeIterator<Item = S>) -> String {
+    String::from(
+        variants
+            .fold(String::from("one of: "), |acc, msg| {
+                format!("{acc} `{msg}',")
+            })
+            .trim_end_matches(','),
+    )
 }
 
 fn node_loc(node: &Node) -> SourceSpan {
     let Range { start, end } = node.byte_range();
 
-    SourceSpan::new(
-	SourceOffset::from(start),
-	SourceOffset::from(end - start)
-    )
+    SourceSpan::new(SourceOffset::from(start), SourceOffset::from(end - start))
 }
 
 // curry?
@@ -80,100 +101,283 @@ fn expect(expected: &'static str, node: &Node) -> Result<SourceSpan> {
     let loc = node_loc(node);
     let actual = node.kind();
     if actual == expected {
-	Ok(loc)
+        Ok(loc)
     } else {
-	Err(mismatch(loc, actual.to_owned(), expected))
+        Err(mismatch(loc, actual.to_owned(), format!("`{expected}'")))
     }
 }
 
 fn parse_source(
     source: impl AsRef<[u8]>,
-    node: &Node
+    node: &Node,
+    read: &mut (impl Read + Seek),
 ) -> Result<syntax::Source<SourceSpan>> {
     let mut statements: Vec<syntax::Statement<SourceSpan>> = vec![];
 
     let loc = expect("source", node)?;
     let mut cursor = node.walk();
     for child in node.children(&mut cursor) {
-	println!("child: {}", child.kind());
-	let stmt = parse_statement(&child)?;
-	statements.push(stmt)
-    };
-    Ok(syntax::Source {
-	source: std::str::from_utf8(source.as_ref())?.to_string(),
-	statements,
-	ann: loc,
-    })
-}
-
-fn parse_choice<T>(
-    variants: &[Parser<T>],
-    node: &Node,
-) -> Result<T> {
-    let mut errs: Vec<ParseError> = vec![];
-    let mut success: Option<T> = None;
-
-    for variant in variants {
-	let res = variant(node);
-	match res {
-	    Err(err) =>
-		errs.push(err),
-	    Ok(t) => {
-		success = Some(t);
-		break;
-	    }
-	}
+        let stmt = parse_statement(&child, read)?;
+        statements.push(stmt)
     }
-
-    success.ok_or_else(|| {
-	let misses =
-	    errs.iter().fold(vec![], |mut acc: Vec<&str>, err| {
-		match err {
-		    ParseError::Mismatch(m) => {
-			acc.push(m.expected);
-			acc
-		    }
-		    _ =>
-			acc
-		}
-	    });
-	let loc = node_loc(node);
-	choice(loc, node.kind().to_owned(), misses)
+    Ok(syntax::Source {
+        source: std::str::from_utf8(source.as_ref())?.to_string(),
+        statements,
+        ann: loc,
     })
 }
 
-fn parse_statement(node: &Node) -> Result<syntax::Statement<SourceSpan>> {
-    parse_choice(&[
-	parse_claim,
-	parse_define,
-	// |node| {
-	//     let expr = parse_expr(node)?;
-	//     Ok(syntax::Statement::Expression(expr))
-	// }
-    ], node)
+fn parse_choice<T, S: Read + Seek>(
+    variants: HashMap<&str, Parser<T, S>>,
+    node: &Node,
+    cursor: &mut S,
+) -> Result<T> {
+    let kind = node.kind();
+    let parser = variants.get(kind).ok_or(mismatch(
+        node_loc(node),
+        String::from(kind),
+        one_of_msg(variants.keys()),
+    ))?;
+
+    parser(node, cursor)
 }
 
-fn parse_claim(node: &Node) -> Result<syntax::Statement<SourceSpan>> {
+fn parse_statement<S: Read + Seek>(
+    node: &Node,
+    cursor: &mut S,
+) -> Result<syntax::Statement<SourceSpan>> {
+    parse_choice(
+        HashMap::from([
+            ("claim", parse_claim as Parser<_, _>),
+            ("define", parse_define),
+            ("expression", parse_expr_stmt),
+        ]),
+        node,
+        cursor,
+    )
+}
+
+fn parse_claim<S: Read + Seek>(
+    node: &Node,
+    cursor: &mut S,
+) -> Result<syntax::Statement<SourceSpan>> {
     let loc = expect("claim", node)?;
-    todo!()
+    let ident = parse_name(node, cursor)?;
+
+    let expr = node
+        .child_by_field_name("type")
+        .ok_or(missing(
+            loc,
+            String::from("`claim'"),
+            String::from("type `expression'"),
+        ))
+        .and_then(|child| parse_expr(&child, cursor))
+        .map(Box::new)?;
+
+    Ok(syntax::Statement::Claim {
+        ann: loc,
+        ident,
+        expr,
+    })
 }
 
-fn parse_define(node: &Node) -> Result<syntax::Statement<SourceSpan>> {
+fn parse_define<S: Read + Seek>(
+    node: &Node,
+    cursor: &mut S,
+) -> Result<syntax::Statement<SourceSpan>> {
     let loc = expect("define", node)?;
-    todo!()
+    let ident = parse_name(node, cursor)?;
+
+    let body = node
+        .child_by_field_name("body")
+        .ok_or(missing(
+            loc,
+            String::from("`define'"),
+            String::from("body `expression'"),
+        ))
+        .and_then(|child| parse_expr(&child, cursor))
+        .map(Box::new)?;
+
+    Ok(syntax::Statement::Define {
+        ann: loc,
+        ident,
+        body,
+    })
 }
 
-fn parse_expr(node: &Node) -> Result<syntax::Expr<SourceSpan>> {
-    todo!()
+fn parse_expr_stmt<S: Read + Seek>(
+    node: &Node,
+    cursor: &mut S,
+) -> Result<syntax::Statement<SourceSpan>> {
+    let expr = parse_expr(node, cursor)?;
+    Ok(syntax::Statement::Expression(expr))
 }
 
-pub fn parse(
-    text: impl AsRef<[u8]> + Clone
-) -> Result<syntax::Source<SourceSpan>> {
+fn parse_expr<S: Read + Seek>(node: &Node, cursor: &mut S) -> Result<syntax::Expr<SourceSpan>> {
+    let loc = expect("expression", node)?;
+    let expr = node.child(0).ok_or(missing(
+        loc,
+        String::from("expression"),
+        String::from("expression body"),
+    ))?;
+    parse_choice(
+        HashMap::from([
+            ("atom", parse_atom as Parser<_, S>),
+            ("lambda", parse_lambda),
+            ("application", parse_application),
+            ("identifier", parse_identifier),
+            ("type_identifier", parse_type_identifier),
+        ]),
+        &expr,
+        cursor,
+    )
+}
+
+fn parse_atom<S: Read + Seek>(node: &Node, cursor: &mut S) -> Result<syntax::Expr<SourceSpan>> {
+    let loc = expect("atom", node)?;
+    let ident = parse_name(node, cursor)?;
+    Ok(syntax::Expr::Atom { ann: loc, ident })
+}
+
+fn parse_lambda<S: Read + Seek>(node: &Node, cursor: &mut S) -> Result<syntax::Expr<SourceSpan>> {
+    let loc = expect("lambda", node)?;
+    let mut ts_cursor = node.walk();
+
+    let args = parse_many(
+        parse_expr,
+        node.children_by_field_name("arguments", &mut ts_cursor),
+        cursor,
+    )?
+    .into_iter()
+    .map(Box::new)
+    .collect();
+
+    let body = node
+        .child_by_field_name("body")
+        .ok_or(missing(
+            loc,
+            String::from("`lambda'"),
+            String::from("body `expression'"),
+        ))
+        .and_then(|child| parse_expr(&child, cursor))
+        .map(Box::new)?;
+
+    Ok(syntax::Expr::Lam {
+        ann: loc,
+        args,
+        body,
+    })
+}
+
+pub fn parse_application(
+    node: &Node,
+    cursor: &mut (impl Read + Seek),
+) -> Result<syntax::Expr<SourceSpan>> {
+    let loc = expect("application", node)?;
+    let mut ts_cursor = node.walk();
+
+    let fun = node
+        .child_by_field_name("function")
+        .ok_or(missing(
+            loc,
+            String::from("function `application'"),
+            String::from("function `expression'"),
+        ))
+        .and_then(|child| parse_expr(&child, cursor))
+        .map(Box::new)?;
+
+    let args = parse_many(
+        parse_expr,
+        node.children_by_field_name("arguments", &mut ts_cursor),
+        cursor,
+    )?
+    .into_iter()
+    .map(Box::new)
+    .collect();
+
+    Ok(syntax::Expr::App {
+        ann: loc,
+        fun,
+        args,
+    })
+}
+
+pub fn parse_identifier(
+    node: &Node,
+    cursor: &mut (impl Read + Seek),
+) -> Result<syntax::Expr<SourceSpan>> {
+    let loc = expect("identifier", node)?;
+    read(loc, cursor).map(|str| syntax::Expr::Var {
+        ann: loc,
+        ident: syntax::Name(str),
+    })
+}
+
+pub fn parse_type_identifier(
+    node: &Node,
+    cursor: &mut (impl Read + Seek),
+) -> Result<syntax::Expr<SourceSpan>> {
+    let loc = expect("type_identifier", node)?;
+    read(loc, cursor).map(|str| syntax::Expr::TyVar {
+        ann: loc,
+        ident: syntax::Name(str),
+    })
+}
+
+pub fn parse_name(node: &Node, cursor: &mut (impl Read + Seek)) -> Result<syntax::Name> {
+    let kind = node.kind();
+    let ident = node.child_by_field_name("identifier").ok_or(missing(
+        node_loc(node),
+        String::from(kind),
+        String::from("identifier"),
+    ))?;
+
+    read(node_loc(&ident), cursor).map(syntax::Name)
+}
+
+pub fn parse_many<'a, T, S: Read + Seek>(
+    parser: Parser<T, S>,
+    nodes: impl Iterator<Item = Node<'a>>,
+    cursor: &mut S,
+) -> Result<Vec<T>> {
+    nodes.map(|node| parser(&node, cursor)).collect()
+}
+
+pub fn parse<T>(text: T) -> Result<syntax::Source<SourceSpan>>
+where
+    T: AsRef<[u8]> + Clone,
+{
     let mut parser = tree_sitter::Parser::new();
     parser.set_language(pie::language())?;
 
     let tree = parser.parse(text.clone(), None).unwrap();
-    println!("debug: {}", tree.root_node().to_sexp());
-    parse_source(text, &tree.root_node())
+    let mut cursor = Cursor::new(text.clone());
+    parse_source(text, &tree.root_node(), &mut cursor)
+}
+
+// extracting data from source
+fn read(loc: SourceSpan, cursor: &mut (impl Read + Seek)) -> Result<String> {
+    let loc_ = loc.offset().try_into().map_err(|err: TryFromIntError| {
+        ParseError::Reading(Reading {
+            loc,
+            help: err.to_string(),
+        })
+    })?;
+
+    cursor.seek(SeekFrom::Start(loc_)).map_err(|err| {
+        ParseError::Reading(Reading {
+            loc,
+            help: err.to_string(),
+        })
+    })?;
+
+    let mut buf: Vec<u8> = vec![0; loc.len()];
+    cursor.read_exact(&mut buf).map_err(|err| {
+        ParseError::Reading(Reading {
+            loc,
+            help: err.to_string(),
+        })
+    })?;
+
+    String::from_utf8(buf).map_err(|err| ParseError::Encoding(err.utf8_error()))
 }
--- lib/syntax/mod.rs
+++ lib/syntax/mod.rs
@@ -1,11 +1,11 @@
 use std::fmt;
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Name(String);
+pub struct Name(pub String);
 
 impl fmt::Display for Name {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-	return write!(f, "{}", self.0);
+        return write!(f, "{}", self.0);
     }
 }
 
@@ -14,24 +14,49 @@ pub struct Atom(Name);
 
 impl fmt::Display for Atom {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-	return write!(f, "'{}", self.0);
+        return write!(f, "'{}", self.0);
     }
 }
 
 #[derive(Clone, Debug)]
 pub enum Expr<Ann = ()> {
-    Atom { ann: Ann, ident: Name },
-    Lam { ann: Ann, ident: Name, body: Box<Expr> },
-    Arr { ann: Ann, domain: Box<Expr>, codomain: Vec<Box<Expr>> },
-    App { ann: Ann, fun: Box<Expr>, arg: Vec<Box<Expr>> },
-    Var { ann: Ann, ident: Name },
-    Type { ann: Ann, ident: Name },
+    Atom {
+        ann: Ann,
+        ident: Name,
+    },
+    Lam {
+        ann: Ann,
+        args: Vec<Box<Expr<Ann>>>,
+        body: Box<Expr<Ann>>,
+    },
+    // Arr { ann: Ann, domain: Box<Expr<Ann>>, codomain: Vec<Box<Expr<Ann>>> },
+    App {
+        ann: Ann,
+        fun: Box<Expr<Ann>>,
+        args: Vec<Box<Expr<Ann>>>,
+    },
+    Var {
+        ann: Ann,
+        ident: Name,
+    },
+    TyVar {
+        ann: Ann,
+        ident: Name,
+    },
 }
 
 #[derive(Clone, Debug)]
 pub enum Statement<Ann = ()> {
-    Claim { ann: Ann, ident: Name, expr: Box<Expr> },
-    Define { ann: Ann, ident: Name, body: Box<Expr> },
+    Claim {
+        ann: Ann,
+        ident: Name,
+        expr: Box<Expr<Ann>>,
+    },
+    Define {
+        ann: Ann,
+        ident: Name,
+        body: Box<Expr<Ann>>,
+    },
     Expression(Expr<Ann>),
 }
 
@@ -39,7 +64,7 @@ pub enum Statement<Ann = ()> {
 pub struct Source<Ann = ()> {
     pub source: String,
     pub statements: Vec<Statement<Ann>>,
-    pub ann: Ann
+    pub ann: Ann,
 }
 
 // given that it is necessary to have claim for every definition, this
